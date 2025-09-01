4b29486cddcb49f217b6c8adc665098d059b33e4
Steven Bruns <steven.bruns@mymail.champlain.edu>
2025-09-01 13:22:35 -0400

    I believe that is the AlignmentRule.cpp done. It was slightly different from what I had on the other side, so I had to change things. I might still have to if things still seem off.

--- examples/flocking/behaviours/AlignmentRule.cpp
+++ examples/flocking/behaviours/AlignmentRule.cpp
@@ -1,12 +1,29 @@
 #include "AlignmentRule.h"
+
+#include "../../../cmake-build-debug/_deps/glm-src/glm/gtc/constants.hpp"
 #include "../gameobjects/Boid.h"
 
 Vector2f AlignmentRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
-  // Try to match the heading of neighbors = Average velocity
-  Vector2f averageVelocity = Vector2f::zero();
-
   // todo: add your code here to align each boid in a neighborhood
+  // Try to match the heading of neighbors = Average velocity
   // hint: iterate over the neighborhood
+
+  if (neighborhood.empty()) {
+    return Vector2f::zero();
+  }
+
+  Vector2f averageVelocity = Vector2f::zero();
+  int count = 0;
+
+  for (Boid* neighbor : neighborhood) {
+    if (neighbor == boid) continue;
+    averageVelocity += neighbor->velocity;
+    count++;
+  }
+
+  if (count == 0) return Vector2f::zero();
+
+  averageVelocity /= (double)count;
 
   return Vector2f::normalized(averageVelocity);
 }
