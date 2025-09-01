59fac4173e77d259f81222fafb09123bcdaa23f9
Steven Bruns <steven.bruns@mymail.champlain.edu>
2025-09-01 13:35:04 -0400

    We have our CohesionRule.cpp working! It's cool to see them bunch up and switch it.

--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -3,11 +3,29 @@
 
 Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
   Vector2f cohesionForce;
-
   // todo: add your code here to make a force towards the center of mass
   // hint: iterate over the neighborhood
-
   // find center of mass
 
-  return cohesionForce;
+  if (neighborhood.empty()) {
+    return Vector2f::zero();
+  }
+
+  Vector2f center= Vector2f::zero();
+  int count = 0;
+
+  for (Boid* neighbor : neighborhood) {
+    if (neighbor == boid) continue;
+    center += neighbor->getPosition();
+    count++;
+  }
+
+  if (count == 0) return Vector2f::zero();
+
+  center /= (double)count;
+  Vector2f desired = center - boid->getPosition();
+
+  return Vector2f::normalized(desired);
+
+  //return cohesionForce;
 }
