117d16edb36ad0ea92f2bf8187bf56fcfbd058a2
Steven Bruns <steven.bruns@mymail.champlain.edu>
2025-09-10 18:09:55 -0400

    Technically functioning with the same code, but obviously has to be altered due to difference in neighbors.

--- examples/life/rules/HexagonGameOfLife.cpp
+++ examples/life/rules/HexagonGameOfLife.cpp
@@ -3,5 +3,38 @@
 //
 
 #include "HexagonGameOfLife.h"
-void HexagonGameOfLife::Step(World& world) {}
-int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) { return 0; }
+void HexagonGameOfLife::Step(World& world) {
+  int sideSize = world.SideSize();
+
+  for (int y = 0; y < sideSize; ++y) {
+    for (int x = 0; x < sideSize; ++x) {
+      Point2D point{x, y};
+      int neighbors = CountNeighbors(world, point);
+      bool alive = world.Get(point);
+      bool nextAlive = false;
+
+      if (alive) {
+        nextAlive = (neighbors == 2 || neighbors == 3);
+      } else {
+        nextAlive = (neighbors == 3);
+      }
+
+      world.SetNext(point, nextAlive);
+    }
+  }
+}
+int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) {
+
+  int count = 0;
+  int sideSize = world.SideSize();
+
+  const int dx[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+  const int dy[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+
+  for (int i = 0; i < 8; ++i) {
+    Point2D neighbor{point.x + dx[i], point.y + dy[i]};
+    if (world.Get(neighbor)) count++;
+  }
+
+  return count;
+}
