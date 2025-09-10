1b17b31db336c75a81b969573a52672d62761eff
Steven Bruns <steven.bruns@mymail.champlain.edu>
2025-09-10 18:34:44 -0400

    Was initially clicking two tiles at once which made me confused on if the neighbors were actually correct or not.

--- examples/life/rules/HexagonGameOfLife.cpp
+++ examples/life/rules/HexagonGameOfLife.cpp
@@ -28,10 +28,27 @@ int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) {
   int count = 0;
   int sideSize = world.SideSize();
 
-  const int dx[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
-  const int dy[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+  //const int dx[6] = {-1, 0, 1, -1, 1, -1};
+  //const int dy[6] = {-1, -1, -1, 0, 0, 1};
 
-  for (int i = 0; i < 8; ++i) {
+  int dx[6], dy[6];         //Even
+  if (point.y % 2 == 0) {
+    dx[0] = -1; dy[0] = -1;
+    dx[1] = 0; dy[1] = -1;
+    dx[2] = -1; dy[2] = 0;
+    dx[3] = 1; dy[3] = 0;
+    dx[4] = -1; dy[4] = 1;
+    dx[5] = 0; dy[5] = 1;
+    } else {                 // ODD
+    dx[0] = 0;  dy[0] = -1;
+    dx[1] = 1;  dy[1] = -1;
+    dx[2] = -1; dy[2] = 0;
+    dx[3] = 1;  dy[3] = 0;
+    dx[4] = 0;  dy[4] = 1;
+    dx[5] = 1;  dy[5] = 1;
+  }
+
+  for (int i = 0; i < 6; ++i) {
     Point2D neighbor{point.x + dx[i], point.y + dy[i]};
     if (world.Get(neighbor)) count++;
   }
