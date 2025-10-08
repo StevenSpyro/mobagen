96b66c7ca2d33788992012164b5080d882a6379f
Steven Bruns <steven.bruns@mymail.champlain.edu>
2025-10-08 03:18:13 -0400

    I've officially trapped the cat!

--- examples/catchthecat/Cat.cpp
+++ examples/catchthecat/Cat.cpp
@@ -1,8 +1,10 @@
 #include "Cat.h"
 #include "World.h"
+#include "Agent.h"
 #include <stdexcept>
 
 Point2D Cat::Move(World* world) {
+  /*
   auto rand = Random::Range(0, 5);
   auto pos = world->getCat();
   switch (rand) {
@@ -21,4 +23,15 @@ Point2D Cat::Move(World* world) {
     default:
       throw "random out of range";
   }
+  */
+
+  std::vector<Point2D> path = Agent::generatePath(world);
+
+  if (!path.empty()) {
+    // Go to exit
+    return path.front();
+  }
+
+  // Trapped?
+  return world->getCat();
 }
--- examples/catchthecat/Catcher.cpp
+++ examples/catchthecat/Catcher.cpp
@@ -1,11 +1,33 @@
 #include "Catcher.h"
 #include "World.h"
+#include "Agent.h"
 
 Point2D Catcher::Move(World* world) {
+  /*
   auto side = world->getWorldSideSize() / 2;
   for (;;) {
     Point2D p = {Random::Range(-side, side), Random::Range(-side, side)};
     auto cat = world->getCat();
     if (cat.x != p.x && cat.y != p.y && !world->getContent(p)) return p;
   }
+  */
+
+  std::vector<Point2D> path = Agent::generatePath(world);
+
+  // Currently places alongside the cat in the pathway
+  if (!path.empty()) {
+    // Block the Kitty
+    Point2D blockPath = path.front();
+
+    if (world->catcherCanMoveToPosition(blockPath)) {
+      return blockPath;
+    }
+  }
+
+  // Other Way to make it harder for the cat
+  auto neighbors = world->getVisitableNeighbors(world->getCat());
+  if (!neighbors.empty()) {
+    return neighbors.front();
+  }
+
 }
