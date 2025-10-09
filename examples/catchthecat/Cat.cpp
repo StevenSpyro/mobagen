872fdb347f932edeeee749c73e3f7ace76b19e11
StevenBruns <steven.bruns@mymail.champlain.edu>
2025-10-09 10:56:29 -0400

    CAT FIX

--- examples/catchthecat/Cat.cpp
+++ examples/catchthecat/Cat.cpp
@@ -4,6 +4,7 @@
 #include <stdexcept>
 
 Point2D Cat::Move(World* world) {
+
   /*
   auto rand = Random::Range(0, 5);
   auto pos = world->getCat();
@@ -30,6 +31,15 @@ Point2D Cat::Move(World* world) {
   if (!path.empty()) {
     // Go to exit
     return path.front();
+  }
+
+  auto neighbors = world->getVisitableNeighbors(world->getCat());
+
+  if (!neighbors.empty()) {
+    Point2D p = neighbors[Random::Range(0, neighbors.size() - 1)];
+    if (world->catCanMoveToPosition(p)) {
+      return p;
+    }
   }
 
   // Trapped?
